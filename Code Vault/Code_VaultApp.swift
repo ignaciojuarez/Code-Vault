f844bdeed5dfbe84e9f15c5767bd12f2d2f913fd
Ignacio Juarez <ignacio@post.com>
2023-09-12 22:32:37 -0700

    Initial Commit

---
+++ Code Vault/Code_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_VaultApp.swift
+//  Code Vault
+//
+//  Created by Ignacio Juarez on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
