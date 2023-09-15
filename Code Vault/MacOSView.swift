2630dff9674560e2b0974f781d7542f82a11b792
Ignacio Juarez <ignacio@post.com>
2023-09-15 14:56:07 -0700

    Update MacOSView.swift
    better side bar

--- Code Vault/MacOSView.swift
+++ Code Vault/MacOSView.swift
@@ -1,47 +1,55 @@
-//
-//  ContentView.swift
-//  Code
-//
-//  Created by Jordan Singer on 9/6/20.
-//
-
 import SwiftUI
 
 struct FileItem: Hashable, Identifiable {
-    var id: Self { self }
+    var id: UUID = UUID()  // Updated to use UUID for unique id
     var name: String
     var image: String
     var children: [FileItem]? = nil
     var selected: Bool = false
 }
 
-let data =
-    FileItem(name: "Code", image: "folder", children:
-    [
-        FileItem(name: "assets", image: "folder", children: []),
-        FileItem(name: "index.html", image: "doc", selected: true),
-        FileItem(name: "style.css", image: "doc"),
-    ])
+let data = FileItem(name: "Code", image: "folder", children: [
+    FileItem(name: "assets", image: "folder", children: []),
+    FileItem(name: "index.html", image: "doc", selected: true),
+    FileItem(name: "style.css", image: "doc"),
+])
 
 struct MacOSView: View {
-    @State var selection: Set<Int> = [0]
-    
+    @State var selection: Set<UUID> = []
+    @State var files: [FileItem] = [data]
+
     var body: some View {
         NavigationView {
             List(selection: self.$selection) {
+                Section(header: Text("Options")) {
+                    Button("All") {}
+                    Button("Trash") {}
+                    Button("Uncategorized") {}
+                }
                 Section(header: Text("Folders")) {
-                    OutlineGroup(data, children: \.children) { item in
-                        if item.selected {
-                            Label(item.name, systemImage: item.image)
-                            .tag(0)
-                        } else {
-                            Label(item.name, systemImage: item.image)
+                    ForEach(files, id: \.id) { file in
+                        OutlineGroup(file, children: \.children) { item in
+                            if item.selected {
+                                Label(item.name, systemImage: item.image)
+                                    .contextMenu {
+                                        Button("New Folder", action: {
+                                            var newFolder = FileItem(name: "New Folder", image: "folder")
+                                            files.append(newFolder)
+                                        })
+                                    }
+                            } else {
+                                Label(item.name, systemImage: item.image)
+                            }
                         }
                     }
                 }
+                Section(header: Text("Tags")) {
+                    // Placeholder for your tags, add items similar to folders
+                    Text("Your Tags Here")
+                }
             }
             .listStyle(SidebarListStyle())
-            .frame(minWidth: 180, idealWidth: 180, maxWidth: 180, maxHeight: .infinity)
+            .frame(minWidth: 200, idealWidth: 250, maxWidth: .infinity, maxHeight: .infinity) // Adjusted resizing values
             
             CodeView()
         }
@@ -50,44 +58,9 @@ struct MacOSView: View {
 
 struct CodeView: View {
     @State var text = ""
-    
+
     var body: some View {
         ZStack {
-            // demo syntax highlighting
-//            HStack {
-//                VStack(alignment: .leading, spacing: 4) {
-//                    Group {
-//                        Text("<!").foregroundColor(.gray) + Text("DOCTYPE").foregroundColor(.purple) + Text(" html").foregroundColor(.orange) + Text(">").foregroundColor(.gray)
-//                    }
-//
-//                    Group {
-//                        Text("<").foregroundColor(.gray) + Text("html").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                        Text("<").foregroundColor(.gray) + Text("head").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                    }
-//
-//                    HStack(spacing: 0) {
-//                        Group {
-//                            Text("    <").foregroundColor(.gray) + Text("title").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                        }
-//                        Group {
-//                            Text("</").foregroundColor(.gray) + Text("title").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                        }
-//                    }
-//
-//                    Group {
-//                        Text("</").foregroundColor(.gray) + Text("head").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                        Text("<").foregroundColor(.gray) + Text("body").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                        Text("")
-//                        Text("</").foregroundColor(.gray) + Text("body").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                        Text("</").foregroundColor(.gray) + Text("html").foregroundColor(.red) + Text(">").foregroundColor(.gray)
-//                    }
-//                    Spacer()
-//                }
-//                Spacer()
-//            }
-//            .padding(8)
-//            .font(.system(.body, design: .monospaced))
-            
             TextEditor(text: $text)
                 .padding(8)
                 .font(.system(.body, design: .monospaced))
